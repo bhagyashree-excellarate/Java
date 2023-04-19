75d035064f488350cb001cf2ed32f621dbe36d15
bhagyashree-excellarate <121869984+bhagyashree-excellarate@users.noreply.github.com>
2023-04-19 11:59:50 +0530

    Added Calculator assignment 2

---
+++ Math.java
@@ -0,0 +1,34 @@
+
+public class Math {
+	
+	public void add(int a, int b) {
+		int result = a+b;
+		System.out.println("The Addition is " + result);
+	}
+
+	public void sub(int a, int b) {
+		int result = a-b;
+		System.out.println("The Substraction is " + result);
+	}
+	
+	public void multiply(int a, int b) {
+		int result = a*b;
+		System.out.println("The Multiplication is " + result);
+	}
+	
+	public void divide(int a, int b) {
+		int result = a/b;
+		System.out.println("The Division is " + result);
+	}
+	
+	public void square(int c) {
+		int result = c*c;
+		System.out.println("The Square is " + result);
+	}
+	
+	public void cube(int c) {
+		int result = c*c*c;
+		System.out.println("The Cube is " + result);
+	}
+}
+
